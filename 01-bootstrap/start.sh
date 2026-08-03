5ac4271d62ef4b095295cc56d74c8f0812a805a1
Almas <alm15@mail.ru>
2026-08-03 11:27:23 +0000

    added master branch config, worked well

--- 01-bootstrap/start.sh
+++ 01-bootstrap/start.sh
@@ -1,3 +1,3 @@
-sudo ansible-playbook -i /home/ubuntu/k8s-2026/01-bootstrap/inventory/production/hosts.ini playbooks/01-controller.yml -vv
-sudo ansible-playbook -i /home/ubuntu/k8s-2026/01-bootstrap/inventory/production/hosts.ini playbooks/02-bootstrap.yml  -vv
+ansible-playbook -i /home/ubuntu/k8s-2026/01-bootstrap/inventory/production/hosts.ini playbooks/01-controller.yml -vv
+ansible-playbook -i /home/ubuntu/k8s-2026/01-bootstrap/inventory/production/hosts.ini playbooks/02-bootstrap.yml  -vv
 
